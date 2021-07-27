fbdbcda51777700a95b755eb64d62a41fc482bd5
mbknu <bouchelot.m@gmail.com>
2021-07-27 15:23:48 +0200

    tetris WIP

---
+++ tetris-vanillajs/tetris.js
@@ -0,0 +1,109 @@
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
+
+context.scale(20, 20);
+
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+/*colision*/
+
+function collide(arena, player) {
+  const [m, o] = [player.matrix, player.pos];
+
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(width, height) {
+  const matrix = [];
+  while (height--) {
+    matrix.push(new Array(width).fill(0));
+  }
+  return matrix;
+}
+
+function draw() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = "red";
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
+  draw();
+  requestAnimationFrame(update);
+}
+
+const arena = createMatrix(12, 20);
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix: matrix,
+};
+
+document.addEventListener("keydown", (event) => {
+  let pressedKey = event.key;
+
+  switch (pressedKey) {
+    case "ArrowLeft":
+      player.pos.x--;
+      break;
+    case "ArrowRight":
+      player.pos.x++;
+      break;
+    case "ArrowDown":
+      playerDrop();
+      break;
+    default:
+      console.log("This key does nothing");
+  }
+});
+
+update();
